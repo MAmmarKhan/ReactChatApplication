28f9b369d91f7ca1173338cf6dc7fae0b29a7b63
Ammar Khan <48694563+SurgentFoxy@users.noreply.github.com>
2021-01-18 17:46:36 -0800

    Added build folder

---
+++ build/precache-manifest.9191910359ab969014b1b21edd1dc036.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763fc705da1a1cf7981c4ea6e64f96d9",
+    "url": "/ReactChatApplication/index.html"
+  },
+  {
+    "revision": "a00322c723642f43ce28",
+    "url": "/ReactChatApplication/static/css/main.95ece5c3.chunk.css"
+  },
+  {
+    "revision": "d7c6128ca3c8a299aabe",
+    "url": "/ReactChatApplication/static/js/2.990ca92c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactChatApplication/static/js/2.990ca92c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a00322c723642f43ce28",
+    "url": "/ReactChatApplication/static/js/main.37a61d0c.chunk.js"
+  },
+  {
+    "revision": "48f7130f945ad16d6f27",
+    "url": "/ReactChatApplication/static/js/runtime-main.8fb19d95.js"
+  }
+]);
